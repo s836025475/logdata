2f1b5c67109d6152456e1efa169e502782193d72
zjc <836025475@qq.com>
2020-08-26 15:24:57 +0800

    添加项目文件。

---
+++ logdata/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logdata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello");
+            NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+             _logger.Fatal("FatalMessage");
+        }
+    }
+}
